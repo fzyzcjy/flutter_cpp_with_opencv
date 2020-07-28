5a509c1cf4579c8db48a36c24e8f85f6e9e4203e
fzyzcjy <117010016@link.cuhk.edu.cn>
2020-07-28 12:15:49 +0800

    add android support

--- ios/Classes/native_add.cpp
+++ ios/Classes/native_add.cpp
@@ -3,6 +3,6 @@
 
 extern "C" __attribute__((visibility("default"))) __attribute__((used))
 int32_t native_add(int32_t x, int32_t y) {
-    Mat m = Mat::zeros(x, y, CV_8UC3);
+    cv::Mat m = cv::Mat::zeros(x, y, CV_8UC3);
     return m.rows + m.cols;
 }
