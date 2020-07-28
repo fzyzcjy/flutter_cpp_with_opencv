1a738d3147fc8f01a3fbe8e9977aac4accdcbe8f
fzyzcjy <117010016@link.cuhk.edu.cn>
2020-07-28 11:45:24 +0800

    follow https://flutter.dev/docs/development/platform-integration/c-interop guide

--- lib/native_with_opencv.dart
+++ lib/native_with_opencv.dart
@@ -1,13 +1,8 @@
-import 'dart:async';
+import 'dart:ffi'; // For FFI
+import 'dart:io'; // For Platform.isX
 
-import 'package:flutter/services.dart';
+final DynamicLibrary nativeAddLib =
+    Platform.isAndroid ? DynamicLibrary.open("libnative_add.so") : DynamicLibrary.process();
 
-class NativeWithOpencv {
-  static const MethodChannel _channel =
-      const MethodChannel('native_with_opencv');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-}
+final int Function(int x, int y) nativeAdd =
+    nativeAddLib.lookup<NativeFunction<Int32 Function(Int32, Int32)>>("native_add").asFunction();
