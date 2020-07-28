ef7b79f0c0fecf6f5e82aa2c66eecb8d4cbe35e1
fzyzcjy <117010016@link.cuhk.edu.cn>
2020-07-28 11:53:53 +0800

    add some c++ code using opencv

--- ios/Classes/native_add.cpp
+++ ios/Classes/native_add.cpp
@@ -1,6 +1,8 @@
 #include <stdint.h>
+#include <opencv2/core.hpp>
 
 extern "C" __attribute__((visibility("default"))) __attribute__((used))
 int32_t native_add(int32_t x, int32_t y) {
-    return x + y;
+    Mat m = Mat::zeros(x, y, CV_8UC3);
+    return m.rows + m.cols;
 }
